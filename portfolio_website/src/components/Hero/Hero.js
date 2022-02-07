d949829981106b77f3302c24be4fcba237e1d3d7
Abdoul Sadikou <sadikouhak@gmail.com>
2022-02-06 21:43:47 -0500

    25:29 to do \n in constants.js review projects

--- portfolio_website/src/components/Hero/Hero.js
+++ portfolio_website/src/components/Hero/Hero.js
@@ -4,10 +4,22 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        My personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      Currently in the last year of a bachelor's degree in computer science at Montreal University,
+      I am particularly interested in software programming, Full Stack Development and Leetcode.
+      I am seeking for 2022 New Grad opportunities that will allow me to expand my knowledge in the workplace and gain experience.
+      Feel free to reach out and connect.
+      </SectionText>
+      <Button onClick={()=>window.location='https://github.com/sahak05'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
