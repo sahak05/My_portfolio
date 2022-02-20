ba10ff492d3b66cca55e025f38870ea4cbaebabc
Abdoul Sadikou <sadikouhak@gmail.com>
2022-02-20 14:20:45 -0500

    36:51

--- portfolio_website/src/components/Projects/Projects.js
+++ portfolio_website/src/components/Projects/Projects.js
@@ -10,12 +10,30 @@ const Projects = () => (
     <SectionDivider/>
     <SectionTitle main> Projects</SectionTitle>
     <GridContainer>
-      {projects.map((project)=>(
-        <div>
-          {project.title}
-          <br/>
-          {project.image}
-        </div>
+      {projects.map(({id, image, title, description, tags, source, visit})=>(
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title={title}></HeaderThree>
+            <Hr/ >
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag,i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+
+          </UtilityList>
+        </BlogCard>
       ))}
     </GridContainer>
   </Section>
--- portfolio_website/src/components/Technologies/Technologies.js
+++ portfolio_website/src/components/Technologies/Technologies.js
@@ -1,11 +1,57 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiCode, DiFirebase, DiReact } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <div>
-    Technologies
+    <Section id="tech">
+
+      <SectionDivider />
+      <br/>
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionText>I've worked with a range 
+        of technologies from back-end to 
+        front-end, some discovered in 
+        my university studies and some 
+        through my research. 
+      </SectionText>
+
+      <List>
+        <ListItem>
+          <DiReact size="3rem" />
+          <ListContainer>
+            <ListTitle>Front-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              React.js
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+
+        <ListItem>
+          <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListTitle>Back-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              Node and Databases
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+
+        <ListItem>
+          <DiCode size="3rem" />
+          <ListContainer>
+            <ListTitle>Others</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              Things that i can complete later
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
   </div>
 );
 
